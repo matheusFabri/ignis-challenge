4d600863eaeedc9fabfbcf647f18d1310f2f33c8
matheusFabri <matheus.fabri@viannasempre.com.br>
2023-05-06 12:55:17 -0300

    finalizado com unsorted pontos e missing rodada

--- classes/Campeonato.js
+++ classes/Campeonato.js
@@ -5,49 +5,63 @@ export class Campeonato {
     this.times = times;
     this.rodada = 0;
     this.jogos = [];
+    this.jogosReturno = [];
   }
 
-  //   setJogos() {
-  //     for (let i = 0; i < this.times.length; i++) {
-  //       //this.rodada = i + 1;
-  //       for (let j = i + 1; j < this.times.length; j++) {
-  //         let jogo = new Jogo(this.times[i], this.times[j]);
-  //         this.jogos.push(jogo.toArray());
-  //         //this.rodada++;
-  //       }
-  //     }
-  //   }
+  setJogos() {
+    for (let i = 0; i < this.times.length; i++) {
+      //this.rodada = i + 1;
+      for (let j = i + 1; j < this.times.length; j++) {
+        let jogo = new Jogo(this.times[i], this.times[j]);
+        this.jogos.push(jogo.toArray());
+        //this.rodada++;
+      }
+    }
+    this.jogosReturno = this.jogos.map((jogo) => {
+      return jogo.reverse();
+    });
+  }
 
-  setJogos() {}
-
-  //fazer math.randow de 0 a 3 e desconsiderar os resultados 2 antes de aplicar
-
-  setPontuacao(){
-    this.jogos.forEach((jogo)=>{
+  setPontuacao() {
+    const randomPoints = (jogo) => {
       let result;
 
-        result = Math.floor(Math.random()*4);
-        if(result == 2){
-          result+=1;
-        }
+      result = Math.floor(Math.random() * 4);
+      if (result == 2) {
+        result += 1;
+      }
 
       jogo[0].pontuacao += result;
-      if(result == 3){
+      if (result == 3) {
         jogo[1].pontuacao += 0;
-      } else if(result == 1){
+      } else if (result == 1) {
         jogo[1].pontuacao += 1;
       } else {
         jogo[1].pontuacao += 3;
       }
+    };
+
+    //Turno
+    this.jogos.forEach((jogo) => {
+      randomPoints(jogo);
+    });
+
+    //Returno
+    this.jogosReturno.forEach((jogo) => {
+      randomPoints(jogo);
     });
   }
 
-  printPontuacao(){
-    this.times.forEach((time)=>{
-      console.log(`${time.pontuacao} - ${time.nome}`);
-    })
-  }
+  ordenaPontuacao() {}
 
+  printPontuacao() {
+    const divPoints = document.createElement("div");
+    divPoints.innerHTML += "<h4>Pontuação Final:</h4>";
+    this.times.forEach((time) => {
+      divPoints.innerHTML += `${time.nome} - ${time.pontuacao} pontos<br>`;
+    });
+    document.querySelector(".form").append(divPoints);
+  }
 
   getJogos() {
     return this.jogos;
@@ -62,11 +76,8 @@ export class Campeonato {
     });
 
     //Returno
-    const jogosReturno = this.jogos.map((jogo) => {
-      return jogo.reverse();
-    });
     div.innerHTML += "<h4>Returno:</h4>";
-    jogosReturno.forEach((jogo) => {
+    this.jogosReturno.forEach((jogo) => {
       div.innerHTML += `${jogo[0].nome} vs ${jogo[1].nome} - ${jogo[0].cidade}<br>`;
     });
     document.querySelector(".form").append(div);
